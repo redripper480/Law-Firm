83cde001a14a5d0e3e5e400a6ba723f0c04f3583
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-02-01 20:23:44 +0500

    Typescript huehuehue

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import 'jquery/src/jquery.js'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import App from './App.vue'
-
-createApp(App).mount('#app')
